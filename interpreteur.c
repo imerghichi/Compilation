4742f001acf90f5fddb39f9035a2eb28261704a0
imerghichi <hypathie14@gmail.com>
2020-02-01 23:16:19 +0000

    pile interpreter

--- interpreteur.c
+++ interpreteur.c
@@ -11,4 +11,9 @@ int main(int argc, char** argv) {
 
 
 void interpreter_pseuo_code(){}
-void interpreter_node_courrant(){}
+void interpreter_node_courrant(){
+    while (pc != node_courrant->ligne.nombre_ligne){
+        if(pc >node_courrant->ligne.nombre_ligne) node_courrant = node_courrant->suivant;
+        else node_courrant = node_courrant->precedant;
+    }
+}
